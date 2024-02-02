70c23f7a8647e42980ed0b280321ff7bbdc6299b
DeibyKwl <deibykwl@gmail.com>
2024-02-02 02:35:36 -0500

    Implemented character creation

--- cards.py
+++ cards.py
@@ -1,8 +1,9 @@
 import pygame
 
 class deck_set(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, card_pos):
         super().__init__()
+        print('nothing')
 
         # Sprite 
 
@@ -10,18 +11,28 @@ class deck_set(pygame.sprite.Sprite):
         # Stats
 
 class player_set(pygame.sprite.Sprite):
-    def __init__(self):
-            super().__init__()
+    def __init__(self, player_class):
+        super().__init__()
+        health = 5
+        self.image = pygame.image.load(f'sprites/player/player_{player_class}_{health}.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (130,180))
+        self.rect = self.image.get_rect(topleft = (545,510))
+        self.player_class = player_class
 
-            # Sprite 
+
+    # Stats
+    def player_update(self, health):
+        self.image = pygame.image.load(f'sprites/player/player_{self.player_class}_{health}.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (130,180))
+        self.rect = self.image.get_rect(topleft = (545,510))
 
 
-            # Stats
+
 
 class enemy_set(pygame.sprite.Sprite):
     def __init__(self):
             super().__init__()
-
+ 
             # Sprite 
 
 
